55d7674ccea0ebfbcd41aa89e8191d98bbe6b690
Eric Davila <erykmynn@gmail.com>
2025-11-10 14:27:23 -0700

    Revert "kaggle-data, config sync, import script fixes in testing for clean install"
    This reverts commit e499518ac8fbc9b6bbcf6ece1ee2ada0ddd606cf.

--- kaggle-data/safe-import-restart.sh
+++ kaggle-data/safe-import-restart.sh
@@ -1,23 +1,21 @@
-#!/bin/bash
-
-# ---  Configuration Variables ---
-MIGRATE_ID="testbed_airbnb_la_listings"
-LIMIT=2500
-TOTAL_RUNS=20
-# -------------------------------
-# Exit immediately if any command fails (Crucial for failure handling)
-set -e
-
-echo "Starting migration for: ${MIGRATE_ID}"
-echo "Running ${TOTAL_RUNS} batches of ${LIMIT} items each."
-echo "Will restart DDEV after every batch to avoid errors."
-echo "-------------------------------------"
-
-   ddev restart
-for i in $(seq 1 $TOTAL_RUNS); do
-   echo "starting $i of $TOTAL_RUNS runs (--limit=$LIMIT)"
-   ddev drush migrate:import "${MIGRATE_ID}" --limit="${LIMIT}"
-   ddev restart
-done
-
-echo "Sequence of $TOTAL_RUNS runs complete"
+#! /bin/bash
+ddev drush migrate:import testbed_airbnb_la_listings --limit=5000
+ddev restart
+ddev drush migrate:import testbed_airbnb_la_listings --limit=5000
+ddev restart
+ddev drush migrate:import testbed_airbnb_la_listings --limit=5000
+ddev restart
+ddev drush migrate:import testbed_airbnb_la_listings --limit=5000
+ddev restart
+ddev drush migrate:import testbed_airbnb_la_listings --limit=5000
+ddev restart
+ddev drush migrate:import testbed_airbnb_la_listings --limit=5000
+ddev restart
+ddev drush migrate:import testbed_airbnb_la_listings --limit=5000
+ddev restart
+ddev drush migrate:import testbed_airbnb_la_listings --limit=5000
+ddev restart
+ddev drush migrate:import testbed_airbnb_la_listings --limit=5000
+ddev restart
+ddev drush migrate:import testbed_airbnb_la_listings --limit=5000
+ddev restart
